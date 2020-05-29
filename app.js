eff957a7c540a54bbd78aaa4ea0c1ece61626171
ceosss <sswarajsamant@gmail.com>
2020-05-29 21:38:59 +0530

    typo correction

--- app.js
+++ app.js
@@ -99,6 +99,6 @@ app.post("/changepwd", (req, res) => {
     });
 });
 
-app.listen(3000 || process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server Started");
 });
